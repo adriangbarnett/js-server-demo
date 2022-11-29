445998cd0e7e5bf34cda99292e34ede72e793bd6
Adrian Barnett <adriangbarnett@gmail.com>
2022-11-29 20:04:25 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+// javascript server
+require('dotenv').config({ path: '.env' })
+const http = require("http");
+
+const requestListener = function (req, res) {
+
+    try {
+
+        switch (req.url) {
+
+            case "/":
+                res.writeHead(200, { 'Content-Type': 'text/html' }); 
+                res.write('<html><body><h1>heading1</h1><p>para text</p></body></html>');
+                res.end();
+                break;
+    
+            case "/ping":
+
+                const responseData = {
+                    code: "200", 
+                    message: "Pong"
+                }
+                const jsonContent = JSON.stringify(responseData);
+        
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.write(jsonContent);
+                res.end();
+                break;
+
+            default:
+                res.writeHead(404);
+                res.end('Invalid Request!');
+                break;
+        }
+
+    } catch (e) {
+        res.end(e.stack); 
+    }
+};
+
+const server = http.createServer(requestListener);
+server.listen(process.env.PORT || process.env.LOCALHOST_PORT, () => {
+    console.log(`Server is running...`);
+});
+
